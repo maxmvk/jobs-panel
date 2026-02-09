d8c8546821710706dedc59d4ce25587caf5ce7b9
maxmvk <maxmvk1993@gmail.com>
2026-02-09 22:38:22 +0100

    [JP-6] Updated job-list and styles

--- client/src/app/features/jobs/components/job-list/job-list.component.ts
+++ client/src/app/features/jobs/components/job-list/job-list.component.ts
@@ -1,14 +1,25 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { Store } from '@ngrx/store';
 import { JobsState } from '../../store/jobs.reducer';
 import { getJobs } from '../../store/jobs.actions';
 import { selectJobs } from '../../store/jobs.selectors';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-job-list',
-  imports: [MatListModule, MatCardModule],
+  imports: [
+    MatListModule, 
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule,
+    DatePipe,
+  ],
   templateUrl: './job-list.component.html',
   styleUrl: './job-list.component.scss',
 })
--- client/src/app/features/jobs/models/job.model.ts
+++ client/src/app/features/jobs/models/job.model.ts
@@ -1,6 +1,11 @@
+export type JobStatus = 'open' | 'in_progress' | 'closed';
+
 export interface IJob {
-  _id: string;
-  name: string;
-  task: string;
-  complete: boolean;
+  id: string;
+  title: string;
+  company: string;
+  description: string;
+  status: JobStatus;
+  location: string;
+  createdAt: string;
 }
